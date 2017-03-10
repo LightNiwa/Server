04fa14f567b26180e27fbbd0e5938072938543e9
ltype <920687062@qq.com>
2017-03-10 03:21:48 +0000

    clear password

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = False
+
+VERSION_CODE = 19
+PRIVATE_PATH = '/git/LightReader-Web/private'
+API_URL = 'https://106.187.44.157'
+API_HEADER = {
+    'X-Api-Key': '31971AA043E72E305B48E006ED0AECC5',
+    'X-Api-Time': '1432010950993',
+    'User-Agent': 'LKNovel-Android-0.0.1',
+    'Host': 'www.linovel.com',
+    'Connection': 'keep-alive'
+}
+
+SECRET_KEY = ''
+
+DATABASE_URI = ''
+DATABASE_CONNECT_OPTIONS = {}
+
+UPLOAD_FOLDER = '/static/image/upload/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
+QN_ACCESS_KEY = ''
+QN_SECRET_KEY = ''
+QN_BUCKET_DOMAIN = ''
+
+del os
+
